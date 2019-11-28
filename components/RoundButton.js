a9363897ef46bb19f54a974ca4a87aa2eee89239
kz-gonzalo-curbelo <57901933+kz-gonzalo-curbelo@users.noreply.github.com>
2019-11-28 16:43:35 -0300

    Create RoundButton.js

---
+++ components/RoundButton.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styled from "styled-components/native";
+import { Icon, Button } from "react-native-ui-kitten";
+import { getStatusBarHeight } from "react-native-status-bar-height";
+
+export const RoundButton = styled(Button)`
+/*      position: absolute;
+      top: ${getStatusBarHeight() + 25}px;
+      right: 25px; */
+      height: 50px;
+      width: 50px;
+      border-radius: 25px;
+    `;
+
+export function PersonOutlineButton(onPressCallback) {
+  return (
+    <RoundButton
+      onPress={onPressCallback}
+      icon={() => <Icon fill="white" name="person-outline" />}
+    />
+  );
+}
+
+export function FlashButton(onPressCallback) {
+  return (
+    <RoundButton
+      onPress={onPressCallback}
+      icon={() => <Icon fill="white" name="flash" />}
+    />
+  );
+}
