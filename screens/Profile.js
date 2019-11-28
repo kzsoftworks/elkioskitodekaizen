ec060bf154ef923b75781d760397f3674a55e19f
kz-gonzalo-curbelo <57901933+kz-gonzalo-curbelo@users.noreply.github.com>
2019-11-28 16:44:46 -0300

    Update Profile.js

--- screens/Profile.js
+++ screens/Profile.js
@@ -6,44 +6,45 @@ import { observer } from "mobx-react";
 import styled from "styled-components/native";
 import { Icon, Button, Text } from "react-native-ui-kitten";
 import { getStatusBarHeight } from "react-native-status-bar-height";
-import * as Google from "expo-google-app-auth";
-import * as firebase from "firebase";
+import { logout } from "../scripts/auth";
 import Container from "../components/Container";
 import PurchasesList from "../components/PurchasesList";
 import authState from "../states/authState";
-import authConfig from "../authConfig";
 
 export default observer(function Profile({ navigation }) {
   return (
     <Container>
       <User>
         <UserName>
-          <Icon name="person-outline"></Icon>
+          <Icon name="person-outline" />
           <Text>{get(authState, "user.name", "")}</Text>
         </UserName>
-        <Button onPress={signOut} size="tiny">
+        <Button
+          onPress={() =>
+            logout(() =>
+              navigation.reset(
+                [NavigationActions.navigate({ routeName: "SignIn" })],
+                0
+              )
+            )
+          }
+          size="tiny"
+        >
           Sign Out
         </Button>
       </User>
       <BackButton
         appearance="ghost"
-        icon={() => <Icon name="arrow-back-outline"></Icon>}
+        icon={() => <Icon name="arrow-back-outline" />}
         onPress={() => navigation.goBack()}
-      ></BackButton>
-      <ScrollView>
-        <PurchasesList></PurchasesList>
-      </ScrollView>
+      />
+      <Container>
+        <ScrollView>
+          <PurchasesList />
+        </ScrollView>
+      </Container>
     </Container>
   );
-
-  async function signOut() {
-    await Google.logOutAsync({
-      accessToken: authState.accessToken,
-      ...authConfig,
-    });
-    firebase.auth().signOut();
-    navigation.reset([NavigationActions.navigate({ routeName: "SignIn" })], 0);
-  }
 });
 
 const User = styled.View`
