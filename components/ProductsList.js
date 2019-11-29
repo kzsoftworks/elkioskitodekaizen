96536e69782824c2cc67e6ca42ee13bd4d252ad0
kz-gonzalo-curbelo <57901933+kz-gonzalo-curbelo@users.noreply.github.com>
2019-11-29 17:05:46 -0300

    Create ProductsList.js

---
+++ components/ProductsList.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, StyleSheet, Text } from "react-native";
+import FirebaseState from "../states/firebaseState";
+import { observer } from "mobx-react";
+import Constants from "expo-constants";
+import { toProductItem } from "./ProductItem";
+import Container from "./Container";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+  },
+});
+
+export default observer(function ProductsList({ navigation }) {
+  const [availableItems, setAvailableItems] = useState(null);
+  const [errorWhileQuerying, setErrorWhileQuerying] = useState(null);
+
+  useEffect(() => {
+    FirebaseState.getAllItems()
+      .then(v => setAvailableItems(v))
+      .catch(e => setErrorWhileQuerying(e || "Unknown error"));
+  }, []);
+
+  if (errorWhileQuerying) {
+    return (
+      <Text>{`An error occurred while retrieving the products.\n${errorWhileQuerying}`}</Text>
+    );
+  } else if (availableItems) {
+    return (
+      <Container style={styles.container}>
+        <FlatList
+          data={availableItems}
+          keyExtractor={item => item.bar_code}
+          renderItem={item => {
+            return item && item.item ? toProductItem(item.item) : {};
+          }}
+          ListEmptyComponent={<Text>There are no available items.</Text>}
+        />
+      </Container>
+    );
+  } else if (availableItems === null || availableItems === undefined) {
+    return <ActivityIndicator size="large" />;
+  } else {
+    return <Text>There are no available items.</Text>;
+  }
+});
