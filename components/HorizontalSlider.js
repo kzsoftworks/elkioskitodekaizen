e3c3c778fa72e3031aa07c286dcd0077b3220656
kz-gonzalo-curbelo <57901933+kz-gonzalo-curbelo@users.noreply.github.com>
2019-11-29 11:21:07 -0300

    Update HorizontalSlider.js

--- components/HorizontalSlider.js
+++ components/HorizontalSlider.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Slider } from "react-native";
 
-export default function HorizontalSlider(onValueChangeCallback) {
+export default function HorizontalSlider(initialValue, onValueChangeCallback) {
   return (
     <Slider
-      style={{ width: 250 }}
+      style={{ width: "80%", height: 50 }}
+      value={initialValue}
       minimumValue={0}
       maximumValue={1}
       //   minimumTrackTintColor="#FFFFFF"
