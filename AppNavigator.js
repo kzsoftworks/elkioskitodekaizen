6e158283133188218cd2e713f6956e37592107f3
kz-gonzalo-curbelo <57901933+kz-gonzalo-curbelo@users.noreply.github.com>
2019-12-02 14:20:34 -0300

    Added Home and ChooseProduct screens to navigator.

--- AppNavigator.js
+++ AppNavigator.js
@@ -3,12 +3,16 @@ import { createStackNavigator } from "react-navigation-stack";
 import ProductScanner from "./screens/ProductScanner";
 import Profile from "./screens/Profile";
 import SignIn from "./screens/SignIn";
+import Home from "./screens/Home";
+import ChooseProduct from "./screens/ChooseProduct";
 
 const AppNavigator = createStackNavigator(
   {
     SignIn: { screen: SignIn },
     ProductScanner: { screen: ProductScanner },
     Profile: { screen: Profile },
+    Home: { screen: Home },
+    ChooseProduct: { screen: ChooseProduct },
   },
   {
     headerMode: "none",
