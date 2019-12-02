866248d519215b516a3915014eeba2070609d513
kz-gonzalo-curbelo <57901933+kz-gonzalo-curbelo@users.noreply.github.com>
2019-12-02 14:23:57 -0300

    Update Profile.js

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,16 +1,16 @@
 import { get } from "lodash";
 import React from "react";
-import { ScrollView } from "react-native";
-import { NavigationActions } from "react-navigation";
+import { ScrollView, View } from "react-native";
 import { observer } from "mobx-react";
 import styled from "styled-components/native";
-import { Icon, Button, Text } from "react-native-ui-kitten";
+import { Text } from "react-native-ui-kitten";
 import { getStatusBarHeight } from "react-native-status-bar-height";
-import { logout } from "../scripts/auth";
 import Container from "../components/Container";
 import PurchasesList from "../components/PurchasesList";
 import authState from "../states/authState";
 import { PersonOutlineButton } from "../components/RoundButton";
+import { navigateToScreen } from "../scripts/navigation";
+import { ArrowBackButton } from "../components/RoundButton";
 
 export default observer(function Profile({ navigation }) {
   return (
@@ -20,25 +20,16 @@ export default observer(function Profile({ navigation }) {
           {PersonOutlineButton(() => {})}
           <Text>{get(authState, "user.name", "")}</Text>
         </UserName>
-        <Button
-          onPress={() =>
-            logout(() =>
-              navigation.reset(
-                [NavigationActions.navigate({ routeName: "SignIn" })],
-                0
-              )
-            )
-          }
-          size="tiny"
-        >
-          Sign Out
-        </Button>
       </User>
-      <BackButton
-        appearance="ghost"
-        icon={() => <Icon name="arrow-back-outline" />}
-        onPress={() => navigation.goBack()}
-      />
+      <View
+        style={{
+          position: "absolute",
+          top: getStatusBarHeight() + 25,
+          left: 25,
+        }}
+      >
+        {ArrowBackButton(() => navigateToScreen(navigation, "Home"))}
+      </View>
       <Container>
         <ScrollView>
           <PurchasesList />
@@ -55,13 +46,4 @@ const User = styled.View`
 `;
 const UserName = styled.View`
   margin-bottom: 15px;
-`;
-
-const BackButton = styled(Button)`
-  position: absolute;
-  top: ${getStatusBarHeight() + 25}px;
-  left: 25px;
-  height: 50px;
-  width: 50px;
-  border-radius: 25px;
 `;
