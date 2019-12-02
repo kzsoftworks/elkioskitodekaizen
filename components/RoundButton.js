4a84f112a6d7a95aa5e47e356ea8a1a895e6d20e
kz-gonzalo-curbelo <57901933+kz-gonzalo-curbelo@users.noreply.github.com>
2019-12-02 14:18:54 -0300

    Added ArrowBackButton.

--- components/RoundButton.js
+++ components/RoundButton.js
@@ -1,16 +1,13 @@
 import React from "react";
 import styled from "styled-components/native";
 import { Icon, Button } from "react-native-ui-kitten";
-import { getStatusBarHeight } from "react-native-status-bar-height";
 
 export const RoundButton = styled(Button)`
-/*      position: absolute;
-      top: ${getStatusBarHeight() + 25}px;
-      right: 25px; */
-      height: 50px;
-      width: 50px;
-      border-radius: 25px;
-    `;
+  margin-left: 10px;
+  height: 50px;
+  width: 50px;
+  border-radius: 25px;
+`;
 
 export function PersonOutlineButton(onPressCallback) {
   return (
@@ -62,6 +59,15 @@ export function PlusSquareOutlineButton(onPressCallback) {
     <RoundButton
       onPress={onPressCallback}
       icon={() => <Icon fill="white" name="plus-square-outline" />}
+    />
+  );
+}
+
+export function ArrowBackButton(onPressCallback) {
+  return (
+    <RoundButton
+      onPress={onPressCallback}
+      icon={() => <Icon fill="white" name="arrow-back-outline" />}
     />
   );
 }
